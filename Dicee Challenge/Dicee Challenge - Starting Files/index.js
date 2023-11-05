26d77488a47bea88577b23796c2d0149b085ab37
Telvo <telvowarrior@gmail.com>
2023-11-06 01:15:43 +0300

    step 6 changed the title to display a winner

--- Dicee Challenge/Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge/Dicee Challenge - Starting Files/index.js
@@ -6,3 +6,11 @@ var imgSrc2 = "./images/dice"+randomNumber2+".png"
 
 document.getElementsByTagName("img")[0].setAttribute("src",imgSrc);
 document.getElementsByTagName("img")[1].setAttribute("src",imgSrc2);
+
+if (randomNumber1>randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+} else if (randomNumber2>randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
+} else {
+    document.querySelector("h1").innerHTML = "Draw!";
+}
