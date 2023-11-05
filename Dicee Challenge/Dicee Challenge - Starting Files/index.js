6d2349e440e7c3bf0d790e7e8a385f20e85353bd
Telvo <telvowarrior@gmail.com>
2023-11-06 00:46:02 +0300

    step 4 change the img to a random dice

--- Dicee Challenge/Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge/Dicee Challenge - Starting Files/index.js
@@ -1,1 +1,3 @@
-var randomNumber1 = Math.floor(Math.random()*(7-1))+1;;
+var randomNumber1 = Math.floor(Math.random()*(7-1))+1;
+var imgSrc = "./images/dice"+randomNumber1+".png"
+document.querySelector("img").setAttribute("src",imgSrc);
