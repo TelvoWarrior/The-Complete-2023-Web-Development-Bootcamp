004a9f9713db0815ab42bda6d84aa7c7b5db7912
Telvo <telvowarrior@gmail.com>
2023-11-06 01:06:10 +0300

    step 5 changed both img elements

--- Dicee Challenge/Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge/Dicee Challenge - Starting Files/index.js
@@ -1,3 +1,8 @@
 var randomNumber1 = Math.floor(Math.random()*(7-1))+1;
+var randomNumber2 = Math.floor(Math.random()*(7-1))+1;
+
 var imgSrc = "./images/dice"+randomNumber1+".png"
-document.querySelector("img").setAttribute("src",imgSrc);
+var imgSrc2 = "./images/dice"+randomNumber2+".png"
+
+document.getElementsByTagName("img")[0].setAttribute("src",imgSrc);
+document.getElementsByTagName("img")[1].setAttribute("src",imgSrc2);
