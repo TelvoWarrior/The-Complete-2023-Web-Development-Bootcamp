8aec4e4a4e8de5a24da182e5efe14647de9f0f03
Telvo <telvowarrior@gmail.com>
2023-11-18 01:59:14 +0300

    NPM task completed

---
+++ Backend/2.3 NPM/index.js
@@ -0,0 +1,7 @@
+import generateName from "sillyname";
+var sillyName = generateName();
+console.log(`That gus's name is: ${sillyName}.`);
+
+import superheroes from "superheroes";
+var superHeroName = superheroes.random();
+console.log(`I am ${superHeroName}!`)
