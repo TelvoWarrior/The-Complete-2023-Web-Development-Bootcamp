d6e8717be8232ef7ff849805fdebc92ad12ea7be
Telvo <telvowarrior@gmail.com>
2023-11-21 01:11:13 +0300

    passing data challenge completed

--- Backend/4.2 Passing Data/index.js
+++ Backend/4.2 Passing Data/index.js
@@ -6,9 +6,19 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+const data = {
+  message: "Enter your name below 👇",
+};
 
-app.post("/submit", (req, res) => {});
+app.get("/", (req, res) => {
+  res.render("index.ejs",data);
+});
+
+app.post("/submit", (req, res) => {
+  var calculations = (req.body["fName"] + req.body["lName"]).length
+  data.message=`There are ${calculations} letters in your name.`
+  res.render("index.ejs",data)
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
