a6bcb595b594479c5d6d553ddbb03b449cd41c08
Telvo <telvowarrior@gmail.com>
2023-11-20 17:12:25 +0300

    Secrets project completed

--- Backend/3.5 Secrets Project/index.js
+++ Backend/3.5 Secrets Project/index.js
@@ -1,3 +1,32 @@
 //To see how the final website should work, run "node solution.js".
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
+import express from "express";
+import {dirname} from "path";
+import {fileURLToPath} from "url";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const thePassword = "ILoveProgramming";
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/public/index.html");
+});
+
+app.post("/check", (req, res) => {
+    console.log(req.body["password"]);
+    if (req.body["password"]===thePassword) {
+        res.sendFile(__dirname+"/public/secret.html");
+    } else {
+        res.send("<h1>YOU WILL BE EXTERMINATED!</h1>")
+    }
+  });
+
+app.listen(port,()=>{
+    console.log(`Server running on port: ${port}`);
+});
